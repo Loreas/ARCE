38c6242d3a90b514b6c70563d898bfa1ad4a896c
MeyerJon <Jonathan.Meyer@student.uantwerpen.be>
2017-05-11 16:00:11 +0200

    Update callscript.cpp

--- callscript.cpp
+++ callscript.cpp
@@ -3,11 +3,12 @@ using namespace std;
 void call_script(string filename) {
 
     string command = "python " + filename;
-
     system(command.c_str());
+
 }
 
 void run_all_scripts(){
+    
     string command = "for f in *.py;do python ""$f""; done";
     system(command.c_str());
 
