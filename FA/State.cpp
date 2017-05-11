92a43f91828d4342d58b2a6f9adef17744d13ba7
Dawid Miroyan <dawid.miroyan@student.uantwerpen.be>
2017-05-11 13:57:16 +0200

    Ik wer gedwongen om State hier op te zetten :(

---
+++ FA/State.cpp
@@ -0,0 +1,52 @@
+//
+// Created by dawid on 13.03.17.
+//
+
+#include "State.h"
+
+State::State() {
+    this->starting = false;
+    this->accepting = false;
+}
+
+State::State(std::string name, bool starting, bool accepting) {
+    this->name.insert(name);
+    this->starting = starting;
+    this->accepting = accepting;
+}
+
+State::State(std::set<std::string> name, bool starting, bool accepting) {
+    this->name = name;
+    this->starting = starting;
+    this->accepting = accepting;
+}
+
+const std::string State::getName() const {
+    std::string fullName = "";
+    for (std::string name: this->name) {
+        fullName += name;
+        if (this->name.find(name) != (--this->name.end()))
+            fullName += ", ";
+    }
+    return fullName;
+}
+
+bool State::isStarting() const {
+    return starting;
+}
+
+bool State::isAccepting() const {
+    return accepting;
+}
+
+void State::setName(const std::set<std::string> &name) {
+    this->name = name;
+}
+
+void State::setStarting(bool starting) {
+    this->starting = starting;
+}
+
+void State::setAccepting(bool accepting) {
+    this->accepting = accepting;
+}
---
+++ FA/State.h
@@ -0,0 +1,31 @@
+//
+// Created by dawid on 13.03.17.
+//
+
+#ifndef AUTOMATATHEORY_STATE_H
+#define AUTOMATATHEORY_STATE_H
+
+#include "iostream"
+#include "set"
+
+class State {
+private:
+    std::set<std::string> name;
+    bool starting;
+    bool accepting;
+public:
+    State();
+    State(std::string name, bool starting, bool accepting);
+    State(std::set<std::string> name, bool starting, bool accepting);
+
+    const std::string getName() const;
+    bool isStarting() const;
+    bool isAccepting() const;
+
+    void setName(const std::set<std::string> &name);
+    void setStarting(bool starting);
+    void setAccepting(bool accepting);
+};
+
+
+#endif //AUTOMATATHEORY_STATE_H
