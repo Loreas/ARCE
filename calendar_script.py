b861bd4ec07424ebed3e2a670789017bcaee5a96
Loreas <Loreas@users.noreply.github.com>
2017-05-19 14:14:26 +0200

    Delete calendar_script.py

--- calendar_script.py
+++
@@ -1,8 +0,0 @@
-import sys
-name = str(sys.argv[0])
-date= sys.argv[1]
-time = sys.argv[2]
-subject = sys.argv[3]
-
-with open(name, 'a') as file: 
-    file.write(str(date)+ ' | ' + str(time) + ' | ' + str(subject))
