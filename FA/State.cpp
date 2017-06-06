1f408e7ff8febafee1b73eba35637113a43a0437
Dawid Miroyan <dawid.miroyan@student.uatwerpen.be>
2017-06-06 15:25:15 +0000

    Nog een fixere tfa

--- FA/State.cpp
+++ FA/State.cpp
@@ -2,6 +2,7 @@
 // Created by dawid on 13.03.17.
 //
 
+#include <algorithm>
 #include "State.h"
 
 State::State() {
@@ -41,9 +42,40 @@ bool State::isAccepting() const {
 }
 
 bool State::containsName(const std::string name) const{
-    if (this->name.find(name) != this->name.end())
-        return true;
-    else return false;
+
+    std::set<std::string> thisName;
+    std::set<std::string> otherName;
+
+    std::string number = "";
+    for (const char& ch: this->getName()) {
+        if (ch == '{' or ch == ' ') continue;
+        else if (ch == '}' or ch == ',') {
+            if (number != "") {
+                thisName.insert(number);
+                number = "";
+            }
+        } else number += ch;
+    }
+    if (number != "") thisName.insert(number);
+
+    number = "";
+    for (const char& ch: name) {
+        if (ch == '{' or ch == ' ') continue;
+        else if (ch == '}' or ch == ',') {
+            if (number != "") {
+                otherName.insert(number);
+                number = "";
+            }
+        } else number += ch;
+    }
+    if (number != "") otherName.insert(number);
+
+    std::set<std::string> intersection;
+    std::set_intersection(thisName.begin(), thisName.end(), otherName.begin(), otherName.end(),
+                          std::inserter(intersection, intersection.begin()));
+
+    bool returnValue = (otherName.size() == intersection.size());
+    return returnValue;
 }
 
 void State::setName(const std::set<std::string> &name) {
