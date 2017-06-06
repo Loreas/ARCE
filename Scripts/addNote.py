372eba0f25ce999e3d9112acf384256d0d0a783d
Loreas <loreas.clonen@student.uantwerpen.be>
2017-06-06 16:12:02 +0200

    added script

--- Scripts/addNote.py
+++ Scripts/addNote.py
@@ -1,1 +1,14 @@
-test
+import sys
+import urllib
+import os
+import datetime
+fro
+
+output = sys.argv[1] + "\n"
+for i in sys.argv[2:]:
+    output += i + " "
+
+
+pastebin_vars = {'api_dev_key': '57fe1369d02477a235057557cbeabaa1', 'api_option': 'paste', 'api_paste_code': output}
+response = urllib.urlopen('http://pastebin.com/api/api_post.php', urllib.urlencode(pastebin_vars))
+url = response.read()
